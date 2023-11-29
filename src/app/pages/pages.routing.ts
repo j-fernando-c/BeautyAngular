e949de266d5d2e79208e9af5b8b6c79be52c7748
Jhon Arley Castañeda Vergara <arleycastaneda22@gmail.com>
2023-11-29 15:15:00 -0500

    merge de ramas

--- src/app/pages/pages.routing.ts
+++ src/app/pages/pages.routing.ts
@@ -1,3 +1,4 @@
+import { ListServicioComponent } from './servicio/list-servicio/list-servicio.component';
 import { AddTurnoComponent } from './turno/add-turno/add-turno.component';
 import { CreateEstilistaComponent } from './estilista/create-estilista/create-estilista.component';
 import { RouterModule, Routes } from '@angular/router';
@@ -17,31 +18,40 @@ import { RolesComponent } from './roles/roles.component';
 import { ConfigComponent } from './config/config.component';
 
 import { AuthGuard } from '../auth.guard';
+import { CreateServicioComponent } from './servicio/create-servicio/create-servicio.component';
 
 
 const routes: Routes = [
   {
-    path: 'dashboard' ,canActivate: [AuthGuard] ,
+    path: 'dashboard'  ,
     component: PagesComponent,
     children: [
-      {path: '', component: DashboardComponent},
+      {path: '', component: DashboardComponent, canActivate: [AuthGuard]},
       {path: 'progress', component: ProgressComponent},
       {path: 'dashboard', component: DashboardComponent,canActivate: [AuthGuard]},
       {path: 'grafica1', component: Grafica1Component,canActivate: [AuthGuard]},
       //Rutas para estilista
-      {path: 'estilista/nuevo', component: CreateEstilistaComponent},
-      {path: 'estilista/list', component: ListEstilistaComponent},
-      {path: 'estilista/edit/:id', component: CreateEstilistaComponent},
+      {path: 'estilista/nuevo', component: CreateEstilistaComponent, canActivate: [AuthGuard]},
+      {path: 'estilista/list', component: ListEstilistaComponent, canActivate: [AuthGuard]},
+      {path: 'estilista/edit/:id', component: CreateEstilistaComponent, canActivate: [AuthGuard]},
       //Rutas para cliente
       {path:'cliente/nuevo', component: CreateClienteComponent},
       {path: 'cliente/list', component: ListClienteComponent,canActivate: [AuthGuard]},
-      {path: 'cliente/edit/:id', component:CreateClienteComponent},
+      {path: 'cliente/edit/:id', component:CreateClienteComponent, canActivate: [AuthGuard]}, 
       //Rutas para Usuario
-      {path: 'usuarios/list', component:ListUsuarioComponent},
-      {path: 'usuarios/nuevo', component:CreateUsuarioComponent},
+      {path: 'usuarios/list', component:ListUsuarioComponent, canActivate: [AuthGuard]},
+      {path: 'usuarios/nuevo', component:CreateUsuarioComponent, canActivate: [AuthGuard]},
       //Rutas para Roles
-      {path: 'roles/list', component:RolesComponent},
-      {path: 'config', component:ConfigComponent},
+      {path: 'roles/list', component:RolesComponent, canActivate: [AuthGuard]},
+      {path: 'config', component:ConfigComponent, canActivate: [AuthGuard]},
+
+      //Ruta servicios
+      {path:'servicio/list', component:ListServicioComponent, canActivate: [AuthGuard]},
+      {path:'servicio/nuevo', component:CreateServicioComponent, canActivate: [AuthGuard]},
+      {path:'servicio/edit/:id', component:CreateServicioComponent, canActivate: [AuthGuard]}
+
+    
+
 
 
 
